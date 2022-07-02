9e65e4e02497c911e3ff9a32eea0af26d22f2b5c
z <42330405+Sfree973@users.noreply.github.com>
2022-07-02 21:28:23 +0800

    Update experiments.py

--- experiments.py
+++ experiments.py
@@ -1,13 +1,6 @@
-#!/usr/bin/env python3
-
-"""
- Copyright (c) 2018, salesforce.com, inc.
- All rights reserved.
- SPDX-License-Identifier: BSD-3-Clause
- For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- 
- Experiment Portal.
-"""
+'''
+ Experiment Portal in MMKGR (Zheng et. al ICDE 2023)
+'''
 
 import copy
 import itertools
